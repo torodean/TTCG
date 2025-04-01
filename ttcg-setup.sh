5994dc02417510352f7c6f6ef29545f939fa9223
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-04-01 15:01:50 -0500

    Create ttcg-setup.sh

---
+++ ttcg-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is still being tested and may still be incomplete.
+# This script will run the setup needed for the TTCG project!
+if [[ $(uname) == Linux ]]; then
+    echo "Running setup for Linux!"
+    sudo apt-get install python3
+    sudo apt-get install python3-pip
+    sudo apt-get install python3-tk
+    sudo apt-get install python3-pil python3-pil.imagetk
+    pip3 install regex
+    pip3 install matplotlib
+    pip3 install pytest
+    pip3 install pandas
+    pip3 install csv
+    pip3 install argparse
+
+else
+    echo "Setup not yet configured for your system!"
+fi
