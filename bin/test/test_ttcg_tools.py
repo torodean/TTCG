614660c4bd4f208caf9035e544c3b5a101ce4517
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-04-04 13:17:44 -0500

    Create test_ttcg_tools.py

---
+++ bin/test/test_ttcg_tools.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+import os
+import sys
+import tempfile
+import pytest
+sys.path.append('../')
+
+# Uncomment these as tests are finished.
+#from ttcg_tools import load_placeholder_values
+#from ttcg_tools import generate_combinations
+#from ttcg_tools import get_command_string
+#from ttcg_tools import check_line_in_file
+#from ttcg_tools import get_relative_path
+#from ttcg_tools import rename_file
+#from ttcg_tools import text_in_placeholder_string
+#from ttcg_tools import deduce_effect_style_from_effect_text
+#from ttcg_tools import has_at_most_one_from_source
+#from ttcg_tools import get_sequence_combinations
+#from ttcg_tools import get_combination_id
+#from ttcg_tools import get_number_id
+#from ttcg_tools import get_index_in_baseN
+#from ttcg_tools import sn_in_list
+#from ttcg_tools import save_sn_to_list
